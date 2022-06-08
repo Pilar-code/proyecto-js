3c18c278df23410e30e295046c231d5eb0c0513a
Pilar Falcone <pilarfelicitasfalcone@gmail.com>
2022-06-08 15:45:09 -0300

    Arreglos de estilos

--- eventos.js
+++ eventos.js
@@ -5,6 +5,7 @@ let compra = document.getElementById('compra')
 function productosEnCarrito(productosStorage) {
     carritoBody.innerHTML = " "
     productosStorage.forEach(element => {
+        let precio = element.cant * element.color;
         carritoBody.innerHTML += `
   <table class="table">
      <thead>
@@ -22,8 +23,9 @@ function productosEnCarrito(productosStorage) {
       <td><img src="./img/${element.img}" class="imagenCarrito" alt="..."></td>
       <td>${element.precio}</td>
       <td>${element.cant}</td>
-      <td>$${element.color}</td>
-      <td><button class="btn" id="restar${element.id}"><img src="https://img.icons8.com/ios-glyphs/30/undefined/filled-trash.png"/></button></td>
+      <td>$${precio}</td>
+      <td><button class="btn" id="eliminar${element.id}"><img src="https://img.icons8.com/ios-glyphs/30/undefined/filled-trash.png"/></button></td>
+      <td><button class="btn" id="sumar${element.id}"><img src="https://img.icons8.com/material-rounded/24/undefined/plus--v1.png"/></button></td>
      </tr>
      </tbody>
   </table>
@@ -45,4 +47,25 @@ compra.addEventListener('click', () =>{
         timer: 3000,
       })
 })
- 
+
+/* productosEnCarrito.forEach(element => {
+document.getElementById(`eliminar${element.id}`).addEventListener('click', () =>{
+   if(arrayProductos.find((producto) => producto.cant === element.cant)) {
+       let menos = arrayProductos.findIndex((producto) => producto.cant === element.cant)
+       arrayProductos[menos].cant--;
+       localStorage.setItem('carrito', JSON.stringify(arrayProductos))
+    }
+   })
+}) */
+
+productosEnCarrito(productosStorage).forEach(element => {
+    document.getElementById(`sumar${element.id}`).addEventListener('click', () =>{
+        if (arrayProductos.find(producto => producto.id == element.id)) {
+            let indice = arrayProductos.findIndex(producto => producto.id == element.id)
+            if(arrayProductos[indice].cant < element.stock) {
+                arrayProductos[indice].cant++;
+            }
+            localStorage.setItem('carrito', JSON.stringify(arrayProductos))
+        }
+    })
+})
